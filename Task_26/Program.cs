36c83718faa1d5dbdd7e616d30a3b727fa23ca35
Konstantin Andreev <konstantin-andr@mail.ru>
2022-05-21 13:02:27 +0500

    число цифр в числе

---
+++ Task_26/Program.cs
@@ -0,0 +1,11 @@
+﻿// принимает число и выдает число цифр в числе
+Console.WriteLine("Введите натуральное число");
+int number = int.Parse(Console.ReadLine());
+int count = 0;
+Console.Write($"Количество цифр в числе {number} = ");
+while(number > 0)
+{
+    number = number / 10;
+    count++;
+}
+Console.WriteLine(count);
