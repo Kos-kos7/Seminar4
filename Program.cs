69099f018e0ab826521ea7666e931a483a22e995
Konstantin Andreev <konstantin-andr@mail.ru>
2022-05-21 12:33:03 +0500

    сумма чисел

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿// принимает число и выдает сумму цифр
+Console.WriteLine("Введите натуральное число");
+int number = int.Parse(Console.ReadLine());
+int sum = 0;
+for (int i = 1; i <= number; i++)
+{
+sum = sum + i;
+}
+Console.WriteLine($"Сумма чисел от 1 до {number} = {sum}");
