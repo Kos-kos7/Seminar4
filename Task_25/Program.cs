05abc7c49eb24584e2125c4d8ef628b10f404dc5
Konstantin Andreev <konstantin-andr@mail.ru>
2022-05-25 15:20:51 +0500

    возводит число А в степень Б

---
+++ Task_25/Program.cs
@@ -0,0 +1,18 @@
+﻿//  Напишите цикл, который принимает на вход
+// два числа (A и B) и возводит число A в натуральную
+// степень B.
+
+Console.WriteLine("Введите первое число");
+int number1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите второе число");
+int number2 = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"Ваши числа {number1} и {number2}");
+
+int result = 1;
+for (int i = 1; i <= number2; i++)
+{
+result = result * number1;
+}
+Console.WriteLine($" {number1} в степени {number2} будет {result}");
