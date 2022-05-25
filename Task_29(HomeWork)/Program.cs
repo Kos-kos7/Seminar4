4bdc45f4cf0da2be37c15480e179826dccefe61a
Konstantin Andreev <konstantin-andr@mail.ru>
2022-05-25 15:01:19 +0500

    создает массив из 8 элементов и выводит на экран

---
+++ Task_29(HomeWork)/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая задаёт массив
+// из 8 элементов и выводит их на экран.
+
+
+int[] nums = new int[8];
+var numRnd = new Random();
+for (int i = 0; i < nums.Length; i++)
+{
+    nums[i] = numRnd.Next(0, 100);
+    Console.Write(nums[i] + " ");
+}
